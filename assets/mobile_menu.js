47bc5831784d437ab118d39e0afa54be129a38b7
MFrandDK <94894199+MFrandDK@users.noreply.github.com>
2022-10-25 02:48:18 +0200

    asd

--- assets/mobile_menu.js
+++ assets/mobile_menu.js
@@ -19,13 +19,4 @@ const navSlide = () => {
 }
 navSlide();
 
-// Slider
-const left = document.getElementById("left-side");
 
-const handleMove = e => {
-  left.style.width = `${e.clientX / window.innerWidth * 100}%`;
-}
-
-document.onmousemove = e => handleMove(e);
-
-document.ontouchmove = e => handleMove(e.touches[0]);
